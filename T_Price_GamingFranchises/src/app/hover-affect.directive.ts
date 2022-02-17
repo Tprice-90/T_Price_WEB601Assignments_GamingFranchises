5fad25fd8d110fa7b50b52d0f6cdd2cbc72a244a
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2022-02-17 12:27:12 -0500

    Finished Assignment4

--- T_Price_GamingFranchises/src/app/hover-affect.directive.ts
+++ T_Price_GamingFranchises/src/app/hover-affect.directive.ts
@@ -6,6 +6,7 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 export class HoverAffectDirective {
   @Input() bold?: string;
   @Input() line?: string;
+  @Input() card?: string;
   constructor(private elm: ElementRef, private elm2: ElementRef) { 
   }
   ngOnInit(): void {
@@ -13,10 +14,12 @@ export class HoverAffectDirective {
   @HostListener('mouseenter') onmouseenter() {
     this.underlineElement(this.line);
     this.boldElement(this.bold);
+    this.borderElement(this.card);
   }
   @HostListener('mouseleave') onmouseleave() {
     this.underlineElement('');
     this.boldElement('');
+    this.borderElement('');
   }
   private underlineElement(effect?: string): void {
     this.elm.nativeElement.style.textDecoration = effect;
@@ -24,5 +27,9 @@ export class HoverAffectDirective {
   private boldElement(effect?: string): void {
     this.elm.nativeElement.style.fontWeight = effect;
   }
-
+  private borderElement(effect?: string): void {
+    if(this.elm.nativeElement.classList.contains('first')) {
+      this.elm.nativeElement.style.border = effect;
+    }
+  }
 }
