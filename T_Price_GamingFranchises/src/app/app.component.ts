e64c1e5e248d9db639e7fe1581d32de0738aaaec
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2022-03-11 18:12:35 -0500

    Got single item observable to work, added msgComp

--- T_Price_GamingFranchises/src/app/app.component.ts
+++ T_Price_GamingFranchises/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Content } from '@angular/compiler/src/render3/r3_ast';
+import { Content } from './helper-files/content-interface';
 import { Component, Input, OnInit } from '@angular/core';
-
+import { GameService } from './services/game.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'T_Price_GamingFranchises';
+  singleItem: Content[]
+  constructor(private gameService: GameService) { this.singleItem = [] }
+  ngOnInit(): void {
+    this.gameService.singleItem(4).subscribe(game => this.singleItem = game);
+  }
 }
 
